fcd2451180e508d62c17969155b2057a3a24a956
Raghav Bansal <raghavb1234@gmail.com>
2024-02-22 12:52:07 -0500

    Edit comment to be not rewards specific

--- src/interfaces/IDripModel.sol
+++ src/interfaces/IDripModel.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 interface IDripModel {
-  /// @notice Returns the drip factor, the percentage of undripped rewards which should drip to stakers, as a wad. For
-  /// example, it there are 100 undripped reward assets and this method returns 1e17, then 100 * 1e17 / 1e18 = 10 assets
-  /// will drip.
+  /// @notice Returns the drip factor, the percentage of of some base amount (e.g. undripped rewards or reserve assets)
+  /// which should drip, as a wad. For example, it there are 100 base amount assets and this method returns 1e17, then
+  /// 100 * 1e17 / 1e18 = 10 assets will drip.
   /// @param lastDripTime_ Timestamp of the last drip
   function dripFactor(uint256 lastDripTime_) external view returns (uint256 dripFactor_);
 }
